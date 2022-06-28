1fef57537565ac5878c3176af2c1b931eb99757d
seumsims <seumworldinbangladesh@gmail.com>
2022-06-28 18:36:26 +0600

    master paginator group by next prev simple navigation completed

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,26 +1,30 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Test</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Custom Group BY Pagination on Laravel 8</title>
 </head>
 <body>
-<div></div>
+
 {{--dd($names)--}}
-<div>
+
     @foreach ($data as $date=> $post)
 
-        <div class="grad1"> @foreach ($post as $item)
+        <div class="grad1">
+            @foreach ($post as $item)
 
-            <h1 class="k_1">
-                {{-- <a> {{dd($item->date)}}</a> --}}
-                <a> {{$item->CurrentDate}}</a>
-                @break
-            </h1>
-        @endforeach</div>
+
+                <h1 class="k_1">
+                    {{-- <a> {{dd($item->date)}}</a> --}}
+                    <a> {{$item->CurrentDate}}</a>
+                    @break
+                </h1>
+
+            @endforeach
+        </div>
+
 
         @foreach ($post as $item)
 
@@ -28,13 +32,24 @@
                 <a> {{$item->name}}</a>
             </h1>
         @endforeach
-    @endforeach
 
+
+    @endforeach
+    @php
+        $page = $data->currentPage();
+        $total = $data->total();
+        $last = $data->lastPage();
+        $next = $page + 1;
+        $prev = $page - 1;
+    @endphp
+    <a href="?page={{$prev}}">Previous page</a>
     {{-- <a href="{{$paginator->nextPageUrl()}}">next</a> --}}
     @for ($i = 1; $i < $data->total(); $i++)
-        <a href="?page={{$i}}"> {{$i}} </a>
+        <a href="?page={{$i}}">{{$i}}</a>
     @endfor
-</div>
+    <a href="?page={{$next}}">Next page</a>
+
+
 
 </body>
 </html>
